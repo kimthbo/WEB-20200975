793df953705cf296d8669369ac5ef9d999bc0cdf
김태현 <kimthbo@naver.com>
2023-06-11 08:48:32 +0000

    ## 2023년 4월 5일 5주차 홈페이지 수정 완료

---
+++ js/hover.js
@@ -0,0 +1,7 @@
+function over(obj) {
+	obj.src="img/googlelogo_color_272x92dp_hover1.png";
+}
+
+function out(obj) {
+	obj.src="img/googlelogo_color_272x92dp_hover2.png";
+}
--- js/search.js
+++ js/search.js
@@ -1,5 +1,21 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
 function search_message(){
+	let search_str = document.querySelector("#search_txt"); // 변수에 저장
    alert("검색을 수행합니다!"); 
+	
+	if(search_str.value.length === 0){ // 문자 길이, 엄격한 비교
+       alert("검색어가 비었습니다. 입력해주세요"); 
+    }
+	else if(search_str.value.includes("123") || search_str.value.includes("456") || search_str.value.includes("789")){
+		 alert("비속어는 사용할 수 없습니다.");
+	}
+	else{
+       alert("검색을 수행합니다!");
+       let text = document.getElementById("search_message").innerHTML = search_str.value;
+       document.querySelector("#form_main").submit();
+    }
 }
+
+
+
